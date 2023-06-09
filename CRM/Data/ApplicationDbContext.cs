1db668e4687144111269d136cec0f094f1f5d76c
AlexWindows <SHZWorking@gmail.com>
2023-06-09 14:55:29 +0600

    Create and init project

---
+++ CRM/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace CRM.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : base(options)
+    {
+    }
+}
