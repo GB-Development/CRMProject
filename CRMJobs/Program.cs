b3bd9ec46cdf6d2680dc18a2000540b9deb6eada
Иван Сафронов <safroniv@mail.ru>
2023-06-10 16:42:38 +0300

    CRMJobs001

---
+++ CRMJobs/Program.cs
@@ -0,0 +1,14 @@
+﻿using Hangfire;
+
+namespace CRMJobs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var jobId = BackgroundJob.Schedule(
+                () => Console.WriteLine("Запуск отложенного задания!"),
+                TimeSpan.FromDays(7));
+        }
+    }
+}
