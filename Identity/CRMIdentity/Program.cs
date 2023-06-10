9c7d482d0f2e20a9af767cc11358d302c7a506ca
EugeneTA <tishkov2005@yandex.ru>
2023-06-11 00:10:43 +0300

    change solution folder structure

---
+++ Identity/CRMIdentity/Program.cs
@@ -0,0 +1,125 @@
+﻿using CRMIdentity;
+using CRMIdentity.Data.Models;
+using Duende.IdentityServer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+using CRMIdentity.Data;
+using CRMIdentity.Services.Profile;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Log.Logger = new LoggerConfiguration()
+                        .WriteTo.Console()
+                        .CreateBootstrapLogger();
+
+        Log.Information("Starting up");
+
+        try
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            #region Logger
+
+            builder.Host.UseSerilog((ctx, lc) => lc
+                .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
+                .Enrich.FromLogContext()
+                .ReadFrom.Configuration(ctx.Configuration));
+
+            #endregion
+
+            builder.Services.AddRazorPages();
+
+            #region Database
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseNpgsql(builder.Configuration.GetConnectionString("IdentityUserDBConnection")));
+
+            #endregion
+
+            #region Identity
+
+            builder.Services.AddIdentity<CRMUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            builder.Services
+                .AddIdentityServer(options =>
+                {
+                    options.Events.RaiseErrorEvents = true;
+                    options.Events.RaiseInformationEvents = true;
+                    options.Events.RaiseFailureEvents = true;
+                    options.Events.RaiseSuccessEvents = true;
+
+                    // see https://docs.duendesoftware.com/identityserver/v6/fundamentals/resources/
+                    options.EmitStaticAudienceClaim = true;
+                })
+                // тестовый x509-сертификат, IdentityServer использует RS256 для подписи JWT
+                .AddDeveloperSigningCredential()
+                .AddInMemoryIdentityResources(Config.IdentityResources)
+                .AddInMemoryApiScopes(Config.ApiScopes)
+                .AddInMemoryClients(Config.Clients)
+                .AddAspNetIdentity<CRMUser>()
+                .AddProfileService<CRMProfileService>();
+
+            builder.Services.AddAuthentication()
+                .AddGoogle(options =>
+                {
+                    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+
+                    // register your IdentityServer with Google at https://console.developers.google.com
+                    // enable the Google+ API
+                    // set the redirect URI to https://localhost:5001/signin-google
+                    options.ClientId = "copy client ID from Google here";
+                    options.ClientSecret = "copy client secret from Google here";
+                });
+
+            #endregion
+
+            var app = builder.Build();
+
+            app.UseSerilogRequestLogging();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseIdentityServer();
+            app.UseAuthorization();
+
+            app.MapRazorPages()
+                .RequireAuthorization();
+
+            // this seeding is only for the template to bootstrap the DB and users.
+            // in production you will likely want a different approach.
+            if (args.Contains("/seed"))
+            {
+                Log.Information("Seeding database...");
+                SeedData.EnsureSeedData(app);
+                Log.Information("Done seeding database. Exiting.");
+                return;
+            }
+
+            app.Run();
+        }
+        catch (Exception ex) when (
+                                    // https://github.com/dotnet/runtime/issues/60600
+                                    ex.GetType().Name is not "StopTheHostException"
+                                    // HostAbortedException was added in .NET 7, but since we target .NET 6 we
+                                    // need to do it this way until we target .NET 8
+                                    && ex.GetType().Name is not "HostAbortedException"
+                                )
+        {
+            Log.Fatal(ex, "Unhandled exception");
+        }
+        finally
+        {
+            Log.Information("Shut down complete");
+            Log.CloseAndFlush();
+        }
+    }
+}
---
+++ Identity/CRMIdentity/SeedData.cs
@@ -0,0 +1,55 @@
+﻿using CRMIdentity.Data;
+using CRMIdentity.Data.Models;
+using IdentityModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+using System.Security.Claims;
+
+namespace CRMIdentity
+{
+    public class SeedData
+    {
+        public static void EnsureSeedData(WebApplication app)
+        {
+            using (var scope = app.Services.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = scope.ServiceProvider.GetService<ApplicationDbContext>();
+                context.Database.Migrate();
+
+                var userMgr = scope.ServiceProvider.GetRequiredService<UserManager<CRMUser>>();
+                var admin = userMgr.FindByNameAsync("alice").Result;
+                if (admin == null)
+                {
+                    admin = new CRMUser
+                    {
+                        UserName = "admin",
+                        Email = "admin@gbcrm.com",
+                        EmailConfirmed = true,
+                    };
+                    var result = userMgr.CreateAsync(admin, "Pass123$").Result;
+                    if (!result.Succeeded)
+                    {
+                        throw new Exception(result.Errors.First().Description);
+                    }
+
+                    result = userMgr.AddClaimsAsync(admin, new Claim[]{
+                            new Claim(JwtClaimTypes.Name, "admin"),
+                            //new Claim(JwtClaimTypes.GivenName, "admin"),
+                            //new Claim(JwtClaimTypes.FamilyName, "admin"),
+                            //new Claim(JwtClaimTypes.WebSite, "http://alice.com"),
+                        }).Result;
+                    if (!result.Succeeded)
+                    {
+                        throw new Exception(result.Errors.First().Description);
+                    }
+                    Log.Debug("admin created");
+                }
+                else
+                {
+                    Log.Debug("admin already exists");
+                }
+            }
+        }
+    }
+}
