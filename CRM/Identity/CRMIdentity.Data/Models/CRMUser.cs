4de2e36ff1cd20a7df5fa38e19b800bfa6a23ad3
EugeneTA <tishkov2005@yandex.ru>
2023-06-10 23:57:14 +0300

    change solution folder structure

--- CRM/Identity/CRMIdentity.Data/Models/CRMUser.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace CRMIdentity.Data.Models
-{
-    public class CRMUser: IdentityUser
-    {
-    }
-}
--- CRM/Identity/CRMIdentity/Pages/Account/Logout/LogoutOptions.cs
+++
@@ -1,9 +0,0 @@
-
-namespace CRMIdentity.Pages.Logout
-{
-    public class LogoutOptions
-    {
-        public static bool ShowLogoutPrompt = true;
-        public static bool AutomaticRedirectAfterSignOut = false;
-    }
-}
