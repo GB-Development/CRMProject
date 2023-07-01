57cda04a916f0d47b30f5c8463dcbc705b1ef200
Юрий Митрохин <lordroads@mail.ru>
2023-07-01 13:00:33 +0300

    Add project, add main page.

---
+++ CRM.WebClient/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using CRM.WebClient.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CRM.WebClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contacts()
+        {
+            return View();
+        }
+        public IActionResult Team()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [Authorize]
+        public IActionResult Login(string redirectUrl)
+        {
+            return View("Index");
+        }
+    }
+}
---
+++ CRM.WebClient/Controllers/WorkflowController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CRM.WebClient.Controllers
+{
+    public class WorkflowController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
